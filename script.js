d0e1d0a6d2634635a07928fdaca45d0183f02340
Lucdomingues <lojalucc@outlook.com>
2022-07-04 23:42:10 -0300

    resolucao de problemas

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 let quadroDePixels = document.getElementById('pixel-board');
+function criadorDePixel() {
+  for (index = 1; index <= 25; index += 1) {
+    let childsQuadro = document.createElement('div');
+    childsQuadro.className = 'pixel';
+    quadroDePixels.appendChild(childsQuadro);
+  }
+}
+criadorDePixel();
