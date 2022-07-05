7354af4f847cf6709d98f88e04f7b9921f43efb0
Lucdomingues <lojalucc@outlook.com>
2022-07-05 15:06:29 -0300

    requisito 6 completed com pendencias

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+//seletor de elementos;
 const quadroDePixels = document.getElementById('pixel-board');
-
+const firstColor = document.getElementsByClassName('black');
+//criador dos pixels;
 function criadorDePixel() {
   for (let index = 1; index <= 25; index += 1) {
     const childsQuadro = document.createElement('div');
@@ -8,3 +10,14 @@ function criadorDePixel() {
   }
 }
 criadorDePixel();
+
+//definir cor preta como inicial;
+window.onload = iniciarBlack = function iniciarBlack() {
+  let quadroFull = quadroDePixels.children;
+  for (let i = 0; i < quadroFull.length; i += 1) {
+    let guardaI = quadroFull[i];
+    guardaI.addEventListener('click', function () {
+      guardaI.style.backgroundColor = 'black';
+    });
+  }
+};
