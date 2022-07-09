a7a87dcd31c0457da16f31dc02fbea098e11b007
Lucdomingues <lojalucc@outlook.com>
2022-07-09 17:03:11 -0300

    requisito 07 completed

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-//seletor de elementos;
 const quadroDePixels = document.getElementById('pixel-board');
-const firstColor = document.getElementsByClassName('black');
-//criador dos pixels;
+const cores = document.getElementsByClassName('color');
+
+// criador dos pixels;
+
 function criadorDePixel() {
   for (let index = 1; index <= 25; index += 1) {
     const childsQuadro = document.createElement('div');
@@ -11,13 +12,22 @@ function criadorDePixel() {
 }
 criadorDePixel();
 
-//definir cor preta como inicial;
-window.onload = iniciarBlack = function iniciarBlack() {
-  let quadroFull = quadroDePixels.children;
-  for (let i = 0; i < quadroFull.length; i += 1) {
-    let guardaI = quadroFull[i];
-    guardaI.addEventListener('click', function () {
-      guardaI.style.backgroundColor = 'black';
+// requisito 07;
+for (let seletor = 0; seletor < cores.length; seletor += 1) {
+  cores[seletor].addEventListener('click', (event) => {
+    const novaCor = document.querySelector('.selected');
+    novaCor.classList.remove('selected');
+    event.target.classList.add('selected');
+  });
+}
+
+// requisito 06;
+
+window.onload = () => {
+  const quadroI = quadroDePixels.children;
+  for (let i = 0; i < quadroI.length; i += 1) {
+    quadroI[i].addEventListener('click', (e) => {
+      e.target.style.backgroundColor = 'black';
     });
   }
 };
